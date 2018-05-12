9462bbf01c27daedfaa3f7fba5d22c2e9fc1b837
richard <richard.liu@kpmg.com>
2018-05-12 20:13:21 +0800

    Initial commit

---
+++ src/main/java/com/goku/coreui/sys/controller/index/impl/IndexControllerImpl.java
@@ -0,0 +1,26 @@
+package com.goku.coreui.sys.controller.index.impl;
+
+import com.goku.coreui.sys.controller.index.IndexController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by nbfujx on 2017/12/25.
+ */
+@Controller
+@RequestMapping("/sys")
+public class IndexControllerImpl implements IndexController {
+
+    @Override
+    @RequestMapping("/index")
+    @RequiresPermissions(value={"sys:index:*"})
+    public String  index(Model model) {
+        return  "sys/index";
+    }
+
+
+
+}
