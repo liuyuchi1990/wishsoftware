5e874dbdb2867148a023ab5ad1d975aaca1edfe2
liwenlong <liwenlong_all@163.com>
2018-05-16 15:23:42 +0800

    奖品页面修改

--- src/main/java/com/goku/coreui/prize/controller/PrizeRestController.java
+++ src/main/java/com/goku/coreui/prize/controller/PrizeRestController.java
@@ -9,6 +9,7 @@ import com.goku.coreui.sys.model.ext.TablePage;
 import com.goku.coreui.sys.util.BreadcrumbUtil;
 import com.goku.coreui.sys.util.DateUtil;
 import com.goku.coreui.sys.util.PageUtil;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
@@ -18,7 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -71,6 +74,7 @@ public class PrizeRestController {
 
 
     @RequestMapping("/save")
+    @RequiresPermissions(value={"prize:query"})
     public String  save(@RequestBody Prize prize){
         int result = prizeService.insert(prize);
         if(result>0) {
@@ -99,6 +103,28 @@ public class PrizeRestController {
             return JSON.toJSONString ("true");
         }else{
             return JSON.toJSONString ("false");
+        }
+    }
+
+
+    /**
+     * 前端暴露接口 扫描二维码 绑定奖品信息
+     * @param prize
+     * @return
+     */
+    @RequestMapping("/bind")
+    public String  bind(@RequestBody Prize prize){
+        prize.setSend_time(new Date());
+        int result = prizeService.edit(prize);
+        Map<String,Object> map = new HashedMap();
+        if(result>0) {
+            map.put("status","success");
+            map.put("msg","绑定成功");
+            return JSON.toJSONString (map);
+        }else{
+            map.put("status","success");
+            map.put("msg","绑定失败");
+            return JSON.toJSONString (map);
         }
     }
 
