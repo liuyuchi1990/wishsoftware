cdfdd222acabc950a18af8f9b8352b7da6717463
richard <richard.liu@kpmg.com>
2018-05-19 15:57:26 +0800

    订单管理

---
+++ src/main/java/com/goku/coreui/device/model/Device.java
@@ -0,0 +1,262 @@
+package com.goku.coreui.device.model;
+
+import java.util.Date;
+
+/**
+ * Created by liwenlong on 2018/5/14.
+ */
+public class Device {
+
+    private String device_id;
+    private String device_name;
+    private String device_address;
+    private String device_status;
+    private Integer cargo_lane_1;
+    private Integer cargo_lane_2;
+    private Integer cargo_lane_3;
+    private Integer cargo_lane_4;
+    private Integer cargo_lane_5;
+    private Integer cargo_lane_6;
+    private Integer cargo_lane_7;
+    private Integer cargo_lane_8;
+    private Integer cargo_lane_9;
+    private Integer cargo_lane_10;
+    private Integer cargo_lane_11;
+    private Integer cargo_lane_12;
+    private Integer cargo_lane_13;
+    private Integer cargo_lane_14;
+    private Integer cargo_lane_15;
+    private Integer cargo_lane_16;
+    private Integer cargo_lane_17;
+    private Integer cargo_lane_18;
+    private Integer cargo_lane_19;
+    private Integer cargo_lane_20;
+    private String create_time;
+    private String update_time;
+    private String user_name;
+    private String update_user_id;
+
+    public String getDevice_id() {
+        return device_id;
+    }
+
+    public void setDevice_id(String device_id) {
+        this.device_id = device_id;
+    }
+
+    public String getDevice_name() {
+        return device_name;
+    }
+
+    public void setDevice_name(String device_name) {
+        this.device_name = device_name;
+    }
+
+    public String getDevice_address() {
+        return device_address;
+    }
+
+    public void setDevice_address(String device_address) {
+        this.device_address = device_address;
+    }
+
+    public String getDevice_status() {
+        return device_status;
+    }
+
+    public void setDevice_status(String device_status) {
+        this.device_status = device_status;
+    }
+
+    public Integer getCargo_lane_1() {
+        return cargo_lane_1;
+    }
+
+    public void setCargo_lane_1(Integer cargo_lane_1) {
+        this.cargo_lane_1 = cargo_lane_1;
+    }
+
+    public Integer getCargo_lane_2() {
+        return cargo_lane_2;
+    }
+
+    public void setCargo_lane_2(Integer cargo_lane_2) {
+        this.cargo_lane_2 = cargo_lane_2;
+    }
+
+    public Integer getCargo_lane_3() {
+        return cargo_lane_3;
+    }
+
+    public void setCargo_lane_3(Integer cargo_lane_3) {
+        this.cargo_lane_3 = cargo_lane_3;
+    }
+
+    public Integer getCargo_lane_4() {
+        return cargo_lane_4;
+    }
+
+    public void setCargo_lane_4(Integer cargo_lane_4) {
+        this.cargo_lane_4 = cargo_lane_4;
+    }
+
+    public Integer getCargo_lane_5() {
+        return cargo_lane_5;
+    }
+
+    public void setCargo_lane_5(Integer cargo_lane_5) {
+        this.cargo_lane_5 = cargo_lane_5;
+    }
+
+    public Integer getCargo_lane_6() {
+        return cargo_lane_6;
+    }
+
+    public void setCargo_lane_6(Integer cargo_lane_6) {
+        this.cargo_lane_6 = cargo_lane_6;
+    }
+
+    public Integer getCargo_lane_7() {
+        return cargo_lane_7;
+    }
+
+    public void setCargo_lane_7(Integer cargo_lane_7) {
+        this.cargo_lane_7 = cargo_lane_7;
+    }
+
+    public Integer getCargo_lane_8() {
+        return cargo_lane_8;
+    }
+
+    public void setCargo_lane_8(Integer cargo_lane_8) {
+        this.cargo_lane_8 = cargo_lane_8;
+    }
+
+    public Integer getCargo_lane_9() {
+        return cargo_lane_9;
+    }
+
+    public void setCargo_lane_9(Integer cargo_lane_9) {
+        this.cargo_lane_9 = cargo_lane_9;
+    }
+
+    public Integer getCargo_lane_10() {
+        return cargo_lane_10;
+    }
+
+    public void setCargo_lane_10(Integer cargo_lane_10) {
+        this.cargo_lane_10 = cargo_lane_10;
+    }
+
+    public Integer getCargo_lane_11() {
+        return cargo_lane_11;
+    }
+
+    public void setCargo_lane_11(Integer cargo_lane_11) {
+        this.cargo_lane_11 = cargo_lane_11;
+    }
+
+    public Integer getCargo_lane_12() {
+        return cargo_lane_12;
+    }
+
+    public void setCargo_lane_12(Integer cargo_lane_12) {
+        this.cargo_lane_12 = cargo_lane_12;
+    }
+
+    public Integer getCargo_lane_13() {
+        return cargo_lane_13;
+    }
+
+    public void setCargo_lane_13(Integer cargo_lane_13) {
+        this.cargo_lane_13 = cargo_lane_13;
+    }
+
+    public Integer getCargo_lane_14() {
+        return cargo_lane_14;
+    }
+
+    public void setCargo_lane_14(Integer cargo_lane_14) {
+        this.cargo_lane_14 = cargo_lane_14;
+    }
+
+    public Integer getCargo_lane_15() {
+        return cargo_lane_15;
+    }
+
+    public void setCargo_lane_15(Integer cargo_lane_15) {
+        this.cargo_lane_15 = cargo_lane_15;
+    }
+
+    public Integer getCargo_lane_16() {
+        return cargo_lane_16;
+    }
+
+    public void setCargo_lane_16(Integer cargo_lane_16) {
+        this.cargo_lane_16 = cargo_lane_16;
+    }
+
+    public Integer getCargo_lane_17() {
+        return cargo_lane_17;
+    }
+
+    public void setCargo_lane_17(Integer cargo_lane_17) {
+        this.cargo_lane_17 = cargo_lane_17;
+    }
+
+    public Integer getCargo_lane_18() {
+        return cargo_lane_18;
+    }
+
+    public void setCargo_lane_18(Integer cargo_lane_18) {
+        this.cargo_lane_18 = cargo_lane_18;
+    }
+
+    public Integer getCargo_lane_19() {
+        return cargo_lane_19;
+    }
+
+    public void setCargo_lane_19(Integer cargo_lane_19) {
+        this.cargo_lane_19 = cargo_lane_19;
+    }
+
+    public Integer getCargo_lane_20() {
+        return cargo_lane_20;
+    }
+
+    public void setCargo_lane_20(Integer cargo_lane_20) {
+        this.cargo_lane_20 = cargo_lane_20;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
+
+    public String getUpdate_user_id() {
+        return update_user_id;
+    }
+
+    public void setUpdate_user_id(String update_user_id) {
+        this.update_user_id = update_user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+}
